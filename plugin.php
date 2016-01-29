0b5d48c2eff57a52aefa2e85580f0e930597b577
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-29 14:11:47 +0000

    Don't use anonymous function in plugin.php

--- plugin.php
+++ plugin.php
@@ -9,22 +9,22 @@
  * License: GPL2+
  */
 
+function plugins_themes_rest_api_init() {
+	$plugins_controller = new WP_REST_Plugins_Controller();
+	$plugins_controller->register_routes();
+
+	$themes_controller = new WP_REST_Themes_Controller();
+	$themes_controller->register_routes();
+}
+
 if ( class_exists( 'WP_REST_Controller' )
 	&& ! class_exists( 'WP_REST_Plugins_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-plugins-controller.php';
-
-	add_action( 'rest_api_init', function() {
-		$plugins_controller = new WP_REST_Plugins_Controller();
-		$plugins_controller->register_routes();
-	} );
 }
 
 if ( class_exists( 'WP_REST_Controller' )
 	&& ! class_exists( 'WP_REST_Themes_Controller' ) ) {
 	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-themes-controller.php';
-
-	add_action( 'rest_api_init', function() {
-		$themes_controller = new WP_REST_Themes_Controller();
-		$themes_controller->register_routes();
-	} );
 }
+
+add_action( 'rest_api_init', plugins_themes_rest_api_init );

630e653d67b063a0e1759145c10f52928bcf2089
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-01-29 15:04:10 +0000

    WIP get_items

--- lib/class-wp-rest-plugins-controller.php
+++ lib/class-wp-rest-plugins-controller.php
@@ -37,11 +37,22 @@ class WP_REST_Plugins_Controller extends WP_REST_Controller {
 	}
 
 	public function get_items_permissions_check( $request ) {
-
+		return true;
 	}
 
 	public function get_items( $request ) {
+		$data = array();
 
+		foreach ( get_plugins() as $obj ) {
+			$plugin = $this->prepare_item_for_response( $obj, $request );
+			if ( is_wp_error( $plugin ) ) {
+				continue;
+			}
+
+			$data[ $obj['Name'] ] = $this->prepare_response_for_collection( $plugin );
+		}
+
+		return rest_ensure_response( $data );
 	}
 
 	public function get_item_permissions_check( $request ) {
@@ -61,11 +72,63 @@ class WP_REST_Plugins_Controller extends WP_REST_Controller {
 	}
 
 	public function prepare_item_for_response( $item, $request ) {
-
+		return $item;
 	}
 
 	public function get_item_schema() {
+		$schema = array(
+			'$schema'              => 'http://json-schema.org/draft-04/schema#',
+			'title'                => 'plugin',
+			'type'                 => 'object',
+			'properties'           => array(
+				'Name'        => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'PluginURI'   => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'Version'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'Description'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'Author'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'AuthorURI'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'TextDomain'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'DomainPath'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'Network'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'Title'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				'AuthorName'     => array(
+					'description'  => __( 'The title for the resource.' ),
+					'type'         => 'string',
+					),
+				),
+			);
 
+		return $this->add_additional_fields_schema( $schema );
 	}
 
 	public function get_collection_params() {
